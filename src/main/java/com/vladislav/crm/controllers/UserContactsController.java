21046e4cd764f14778a6da3f61113e958799987b
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-09-25 14:25:25 +0300

    save

--- src/main/java/com/vladislav/crm/controllers/UserContactsController.java
+++ src/main/java/com/vladislav/crm/controllers/UserContactsController.java
@@ -1,10 +1,15 @@
 package com.vladislav.crm.controllers;
 
+import com.vladislav.crm.controllers.assemblers.ReadUserContactsResponseAssembler;
 import com.vladislav.crm.controllers.responses.ReadUserContactsResponse;
 import com.vladislav.crm.entities.User;
 import com.vladislav.crm.services.operations.ReadUserContactsOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.LinkRelation;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,22 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/v1/contacts")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserContactsController {
 
     private final ReadUserContactsOperation readUserContactsOperation;
+    private final ReadUserContactsResponseAssembler readUserContactsResponseAssembler;
 
-    @GetMapping(value = {"", "/"})
-    public List<ReadUserContactsResponse> readUserContacts(Authentication authentication) {
+    @GetMapping(value = {"", "/"})  // todo: спросить нормально ли так делать?
+    public RepresentationModel<?> readUserContacts(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
-        return readUserContactsOperation.execute(user.getId()).stream()
-                .map(contact -> new ReadUserContactsResponse()
-                        .setContactId(contact.getId())
-                        .setName(contact.getName())
-                        .setCompany(contact.getCompany())
-                )
+
+        final List<EntityModel<ReadUserContactsResponse>> models = readUserContactsOperation.execute(user.getId())
+                .stream()
+                .map(readUserContactsResponseAssembler::toModel)
                 .collect(Collectors.toUnmodifiableList());
+        
+        return HalModelBuilder.emptyHalModel()
+                .embed(models, LinkRelation.of("contacts"))
+                .link(linkTo(methodOn(UserContactsController.class).readUserContacts(authentication)).withSelfRel())
+                .build();
     }
+
+//    @GetMapping("/{id}")
+//    public Contact readContact(Authentication authentication, @PathVariable("id") Long id) {
+//        User user = (User) authentication.getPrincipal();
+//
+//    }
 }
