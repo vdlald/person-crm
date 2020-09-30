5842d25477b5d7e18773e9e27f90d47964983b32
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-09-30 19:01:53 +0300

    add ReadPipelineResponse

---
+++ src/main/java/com/vladislav/crm/controllers/responses/ReadPipelineResponse.java
@@ -0,0 +1,28 @@
+package com.vladislav.crm.controllers.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class ReadPipelineResponse {
+
+    private Long id;
+    private String name;
+    private List<StatusResponse> statuses = new ArrayList<>();
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Accessors(chain = true)
+    public static class StatusResponse {
+        private Long id;
+    }
+}
