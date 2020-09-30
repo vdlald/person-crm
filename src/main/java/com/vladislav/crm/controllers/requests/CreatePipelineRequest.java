7f517d4f3eb130d397c1c8c065f042a9ea93ff1a
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-09-30 20:31:22 +0300

    add CreatePipelineRequest

---
+++ src/main/java/com/vladislav/crm/controllers/requests/CreatePipelineRequest.java
@@ -0,0 +1,16 @@
+package com.vladislav.crm.controllers.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class CreatePipelineRequest {
+
+    private String name;
+
+}
