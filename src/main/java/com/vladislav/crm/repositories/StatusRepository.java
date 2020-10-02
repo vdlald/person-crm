cc4fafb0d810abcd434f8182d86fb2ed6dbd4540
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-02 21:32:57 +0300

    add ReadStatusOperation

---
+++ src/main/java/com/vladislav/crm/repositories/StatusRepository.java
@@ -0,0 +1,9 @@
+package com.vladislav.crm.repositories;
+
+import com.vladislav.crm.entities.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StatusRepository extends JpaRepository<Status, Long> {
+}
