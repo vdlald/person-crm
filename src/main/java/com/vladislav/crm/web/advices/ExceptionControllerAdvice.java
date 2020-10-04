a518e2a37edd8ba650d1bb0552af1bfbaf6cb6af
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-04 18:39:52 +0300

    refactor advice

--- src/main/java/com/vladislav/crm/web/advices/ExceptionControllerAdvice.java
+++ src/main/java/com/vladislav/crm/web/advices/ExceptionControllerAdvice.java
@@ -1,12 +1,14 @@
 package com.vladislav.crm.web.advices;
 
 import com.vladislav.crm.web.responses.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
@@ -14,7 +16,7 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse exceptionHandler(Exception e) {
-        e.printStackTrace();
+        log.error(e.getLocalizedMessage(), e);
         return ErrorResponse.builder()
                 .setStatus(HttpStatus.INTERNAL_SERVER_ERROR)
                 .setMessage(e.getMessage())
