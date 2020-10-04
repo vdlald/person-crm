79802101a5e96edf8092e182fa5fdf4e6c993417
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-04 11:53:22 +0300

    contact validation

--- src/main/java/com/vladislav/crm/entities/Company.java
+++ src/main/java/com/vladislav/crm/entities/Company.java
@@ -4,6 +4,7 @@ import lombok.*;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +20,9 @@ import java.util.List;
 @AttributeOverride(name = "id", column = @Column(name = "company_id", updatable = false, nullable = false))
 public class Company extends AbstractEntity {
 
+    @NotBlank
     @Size(min = 1, max = 32)
-    @Column(name = "name", length = 32)
+    @Column(name = "name", length = 32, nullable = false)
     private String name;
 
     @Setter(AccessLevel.PRIVATE)
