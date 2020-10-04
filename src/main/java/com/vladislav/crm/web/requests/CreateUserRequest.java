600828a3653faaf1621ff56c17ab740313918fca
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-04 18:27:48 +0300

    user validation

--- src/main/java/com/vladislav/crm/web/requests/CreateUserRequest.java
+++ src/main/java/com/vladislav/crm/web/requests/CreateUserRequest.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -16,7 +16,7 @@ public class CreateUserRequest {
     @Pattern(regexp = "^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
     private String username;
 
-    @NotEmpty
+    @NotBlank
     @Size(min = 8, max = 64)
     private String password;
 
