572aaa2407957a88c412c3cc3afd7fbbb36519ff
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-04 18:31:09 +0300

    refactor CreateUserOperation

--- src/main/java/com/vladislav/crm/services/operations/users/impl/CreateUserOperationImpl.java
+++ src/main/java/com/vladislav/crm/services/operations/users/impl/CreateUserOperationImpl.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityExistsException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -21,7 +20,9 @@ public class CreateUserOperationImpl implements CreateOperation<User> {
 
     @Override
     public User execute(User newUser) {
-        checkExistUser(newUser.getUsername());
+        if (userRepository.existsByUsername(newUser.getUsername())) {
+            throw new EntityExistsException();
+        }
 
         newUser.setInfo(new UserInfo());
         encodePassword(newUser);
@@ -31,12 +32,5 @@ public class CreateUserOperationImpl implements CreateOperation<User> {
 
     private void encodePassword(User newUser) {
         newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
-    }
-
-    private void checkExistUser(String username) {
-        final Optional<User> byUsername = userRepository.findByUsername(username);
-        if (byUsername.isPresent()) {
-            throw new EntityExistsException();
-        }
     }
 }
