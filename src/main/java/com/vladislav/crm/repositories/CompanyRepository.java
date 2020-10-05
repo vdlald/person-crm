929a67532380c70814c91387c4513d9e102fcaed
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-05 21:51:08 +0300

    add UserOwnsCompanyAuthorization

--- src/main/java/com/vladislav/crm/repositories/CompanyRepository.java
+++ src/main/java/com/vladislav/crm/repositories/CompanyRepository.java
@@ -2,8 +2,14 @@ package com.vladislav.crm.repositories;
 
 import com.vladislav.crm.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    @Query("SELECT user.id from Company where id = :id")
+    Optional<Long> findUserIdById(Long id);
 }
