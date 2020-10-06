5791887edc753041844c438616a788d424d10cc3
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-06 20:50:48 +0300

    logo on start

--- src/main/java/com/vladislav/crm/CrmApplication.java
+++ src/main/java/com/vladislav/crm/CrmApplication.java
@@ -1,15 +1,24 @@
 package com.vladislav.crm;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.nio.file.Files;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class CrmApplication {
+public class CrmApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CrmApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(new String(Files.readAllBytes(new ClassPathResource("logo").getFile().toPath())));
+    }
 }
