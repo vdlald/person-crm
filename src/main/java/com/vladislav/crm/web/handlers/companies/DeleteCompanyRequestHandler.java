2c5df6a0122de05da7bbb1264e3e820973b72231
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-06 08:05:00 +0300

    move file

--- src/main/java/com/vladislav/crm/web/handlers/companies/DeleteCompanyRequestHandler.java
+++ src/main/java/com/vladislav/crm/web/handlers/companies/DeleteCompanyRequestHandler.java
@@ -1,4 +1,4 @@
-package com.vladislav.crm.web.handlers.companies.impl;
+package com.vladislav.crm.web.handlers.companies;
 
 import com.vladislav.crm.web.handlers.RequestHandler;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/vladislav/crm/web/handlers/companies/impl/DeleteCompanyRequestHandlerImpl.java
+++ src/main/java/com/vladislav/crm/web/handlers/companies/impl/DeleteCompanyRequestHandlerImpl.java
@@ -3,6 +3,7 @@ package com.vladislav.crm.web.handlers.companies.impl;
 import com.vladislav.crm.entities.Company;
 import com.vladislav.crm.services.operations.DeleteOperation;
 import com.vladislav.crm.web.handlers.AbstractDeleteEntityRequestHandler;
+import com.vladislav.crm.web.handlers.companies.DeleteCompanyRequestHandler;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 
