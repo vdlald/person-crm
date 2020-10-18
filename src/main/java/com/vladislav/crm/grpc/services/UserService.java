9d74fc631b5525ad23ac3ae07fbc7f9549afaffd
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-18 16:51:46 +0300

    add getUser request

--- src/main/java/com/vladislav/crm/grpc/services/UserService.java
+++ src/main/java/com/vladislav/crm/grpc/services/UserService.java
@@ -1,12 +1,17 @@
 package com.vladislav.crm.grpc.services;
 
 import com.proto.users.*;
+import com.vladislav.crm.entities.User;
+import com.vladislav.crm.entities.UserInfo;
 import com.vladislav.crm.grpc.handlers.users.CreateUserRequestHandler;
 import com.vladislav.crm.grpc.handlers.users.CurrentUserRequestHandler;
+import com.vladislav.crm.services.operations.ReadOperation;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -14,6 +19,7 @@ public class UserService extends UserServiceGrpc.UserServiceImplBase {
 
     private final CurrentUserRequestHandler currentUserRequestHandler;
     private final CreateUserRequestHandler createUserRequestHandler;
+    private final ReadOperation<User> userReadOperation;
 
     @Override
     public void currentUser(CurrentUserRequest request, StreamObserver<CurrentUserResponse> responseObserver) {
@@ -28,6 +34,27 @@ public class UserService extends UserServiceGrpc.UserServiceImplBase {
         final CreateUserResponse response = createUserRequestHandler.handle(request);
 
         responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getUser(GetUserRequest request, StreamObserver<GetUserResponse> responseObserver) {
+        final User user = userReadOperation.execute(request.getUserId());
+        final UserInfo info = user.getInfo();
+
+        responseObserver.onNext(
+                GetUserResponse.newBuilder()
+                        .setUser(UserResponse.newBuilder()
+                                .setId(user.getId())
+                                .setUsername(user.getUsername())
+                                .build())
+                        .setInfo(UserInfoResponse.newBuilder()
+                                .setEmail(Objects.requireNonNullElse(info.getEmail(), ""))
+                                .setFirstname(Objects.requireNonNullElse(info.getFirstname(), ""))
+                                .setLastname(Objects.requireNonNullElse(info.getLastname(), ""))
+                                .build())
+                        .build()
+        );
         responseObserver.onCompleted();
     }
 }
