9b207721935fca5f63c2a7b38d5e0feb6cd1a652
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-25 14:09:43 +0300

    refactor get user stub

--- src/main/java/com/vladislav/crm/services/operations/users/impl/GetCurrentUserStubOperationImpl.java
+++ src/main/java/com/vladislav/crm/services/operations/users/impl/GetCurrentUserStubOperationImpl.java
@@ -8,16 +8,20 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class GetCurrentUserStubOperationImpl implements GetCurrentUserStubOperation {
 
+    private final EntityManager entityManager;
+
     @Override
     public User execute() {
-        return (User) Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+        final User principal = (User) Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
                 .orElseThrow(() -> new AccessDeniedException("No credentials was provided"))
                 .getPrincipal();
+        return entityManager.getReference(User.class, principal.getId());
     }
 }
