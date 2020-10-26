36d846e90bb49b3a61a1f8884f26efe1c20a1474
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-26 20:17:36 +0300

    refactor JwtFilter

--- src/main/java/com/vladislav/crm/JwtFilter.java
+++ src/main/java/com/vladislav/crm/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.vladislav.crm;
 
+import com.vladislav.crm.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtParser;
@@ -7,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,13 +16,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
 @Component("jwtFilter")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class JwtFilter extends OncePerRequestFilter {
 
-    private final UserDetailsService userDetailsService;
     private final JwtParser jwtParser;
 
     @Override
@@ -39,9 +38,13 @@ public class JwtFilter extends OncePerRequestFilter {
             final Jws<Claims> jws = jwtParser.parseClaimsJws(token);
             final Claims claims = jws.getBody();
 
-            final UserDetails userDetails = userDetailsService.loadUserByUsername(claims.get("username", String.class));
+            final User user = new User();
+            user.setId(claims.get("userId", Long.class));
+            user.setUsername(claims.get("username", String.class));
+            user.setAuthorities((List<User.Authority>) claims.get("authorities"));
+
             final UsernamePasswordAuthenticationToken auth =
-                    new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+                    new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
         filterChain.doFilter(request, response);
