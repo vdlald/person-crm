c4fb3ad4b7a9e905458dec243b28465051ddfb20
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-26 20:50:11 +0300

    add parseToken method

--- src/main/java/com/vladislav/crm/JwtFilter.java
+++ src/main/java/com/vladislav/crm/JwtFilter.java
@@ -1,13 +1,11 @@
 package com.vladislav.crm;
 
-import com.vladislav.crm.entities.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.JwtParser;
+import com.vladislav.crm.services.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,14 +14,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 
 @Component("jwtFilter")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class JwtFilter extends OncePerRequestFilter {
 
-    private final JwtParser jwtParser;
+    private final TokenService tokenService;
 
     @Override
     protected void doFilterInternal(
@@ -35,16 +32,12 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if (Objects.nonNull(requestTokenHeader) && requestTokenHeader.startsWith("Bearer ")) {
             final String token = requestTokenHeader.substring(7);
-            final Jws<Claims> jws = jwtParser.parseClaimsJws(token);
-            final Claims claims = jws.getBody();
 
-            final User user = new User();
-            user.setId(claims.get("userId", Long.class));
-            user.setUsername(claims.get("username", String.class));
-            user.setAuthorities((List<User.Authority>) claims.get("authorities"));
+            final UserDetails user = tokenService.parseToken(token);
 
             final UsernamePasswordAuthenticationToken auth =
                     new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
         filterChain.doFilter(request, response);
--- src/main/java/com/vladislav/crm/services/impl/TokenServiceImpl.java
+++ src/main/java/com/vladislav/crm/services/impl/TokenServiceImpl.java
@@ -2,20 +2,27 @@ package com.vladislav.crm.services.impl;
 
 import com.vladislav.crm.entities.User;
 import com.vladislav.crm.services.TokenService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TokenServiceImpl implements TokenService {
+
+    private final JwtParser jwtParser;
 
     @Override
     public String generateToken(User user) {
@@ -31,5 +38,18 @@ public class TokenServiceImpl implements TokenService {
                 .setExpiration(Date.from(expiredAt))
                 .signWith(Keys.hmacShaKeyFor("secretsecretsecretsecretsecretsecret".getBytes(StandardCharsets.UTF_8)))  // todo: вынести secret в application.yaml
                 .compact();
+    }
+
+    @Override
+    public UserDetails parseToken(String token) {
+        final Jws<Claims> jws = jwtParser.parseClaimsJws(token);
+        final Claims claims = jws.getBody();
+
+        final User user = new User();
+        user.setId(claims.get("userId", Long.class));
+        user.setUsername(claims.get("username", String.class));
+        user.setAuthorities((List<User.Authority>) claims.get("authorities"));
+
+        return user;
     }
 }
