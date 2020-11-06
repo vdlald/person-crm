5064ea6a412d9b18155d0b807eb8968d957aa522
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-11-06 21:33:35 +0300

    migrate to mysql

--- src/main/resources/db/changelog/v-1.0/2020-11-06--03-add-refresh-tokens.sql
+++ src/main/resources/db/changelog/v-1.0/2020-11-06--03-add-refresh-tokens.sql
@@ -3,12 +3,11 @@
 --changeset vdlald:28
 create table refresh_tokens
 (
-    token       varchar(36) not null,
-    valid_until timestamp   not null,
-    user_id     bigint      not null,
-    primary key (token)
+    token       varchar(36) not null primary key ,
+    valid_until timestamp   not null default now(),
+    user_id     bigint      not null
 );
 
 --changeset vdlald:29
 alter table refresh_tokens
-    add constraint refresh_tokens_fk_user_id foreign key (user_id) references users
+    add constraint refresh_tokens_fk_user_id foreign key (user_id) references users(user_id)
