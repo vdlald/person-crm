c0fb396a46499c64aa34e92dbce1ad39910a0124
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-11-06 20:02:56 +0300

    refactor

--- src/main/java/com/vladislav/crm/communications/handlers/auth/impl/RefreshAccessTokenRequestHandlerImpl.java
+++ src/main/java/com/vladislav/crm/communications/handlers/auth/impl/RefreshAccessTokenRequestHandlerImpl.java
@@ -4,13 +4,15 @@ import com.vladislav.crm.communications.handlers.auth.RefreshAccessTokenRequestH
 import com.vladislav.crm.communications.responses.AuthResponse;
 import com.vladislav.crm.entities.RefreshToken;
 import com.vladislav.crm.entities.User;
-import com.vladislav.crm.repositories.RefreshTokenRepository;
 import com.vladislav.crm.services.TokenService;
+import com.vladislav.crm.services.operations.refreshtokens.DeleteRefreshTokenOperation;
+import com.vladislav.crm.services.operations.refreshtokens.ReadRefreshTokenOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -19,14 +21,19 @@ import java.util.UUID;
 public class RefreshAccessTokenRequestHandlerImpl implements RefreshAccessTokenRequestHandler {
 
     private final TokenService tokenService;
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final ReadRefreshTokenOperation readRefreshTokenOperation;
+    private final DeleteRefreshTokenOperation deleteRefreshTokenOperation;
 
     @Override
     public AuthResponse handle(UUID refreshTokenRaw) {
-        final RefreshToken refreshToken = refreshTokenRepository.findById(refreshTokenRaw)
-                .orElseThrow(() -> new AccessDeniedException("Refresh token not found"));
+        final RefreshToken refreshToken;
+        try {
+            refreshToken = readRefreshTokenOperation.execute(refreshTokenRaw);
+        } catch (EntityNotFoundException e) {
+            throw new AccessDeniedException("Refresh token not found");
+        }
 
-        refreshTokenRepository.delete(refreshToken);
+        deleteRefreshTokenOperation.execute(refreshToken);
 
         final LocalDateTime now = LocalDateTime.now();
         if (refreshToken.getValidUntil().isAfter(now)) {
