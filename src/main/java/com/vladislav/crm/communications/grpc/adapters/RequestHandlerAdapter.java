aadcee691c41bf78a687e1b8bb6814157f813c59
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-11-09 10:27:32 +0300

    remove refactor candidates

--- src/main/java/com/vladislav/crm/communications/grpc/adapters/RequestHandlerAdapter.java
+++ src/main/java/com/vladislav/crm/communications/grpc/adapters/RequestHandlerAdapter.java
@@ -4,8 +4,5 @@ import com.google.protobuf.GeneratedMessageV3;
 
 public interface RequestHandlerAdapter<REQ extends GeneratedMessageV3, RES extends GeneratedMessageV3> {
 
-    // refactor candidate: может быть здесь стоит принимать и StreamObserver чтобы кидать в него эксепшены?
-    // Или делать много interceptor-ов ?
-    // Мне больше нравится вариант с interceptor
     RES handle(REQ request);
 }
