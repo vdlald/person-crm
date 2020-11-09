b5b5eacc39a53344b3f746175a9acc82799dc9ea
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-11-09 21:13:42 +0300

    refactor DeleteCompanyOperation

--- src/main/java/com/vladislav/crm/services/operations/companies/impl/DeleteCompanyOperationImpl.java
+++ src/main/java/com/vladislav/crm/services/operations/companies/impl/DeleteCompanyOperationImpl.java
@@ -1,6 +1,7 @@
 package com.vladislav.crm.services.operations.companies.impl;
 
 import com.vladislav.crm.entities.Company;
+import com.vladislav.crm.repositories.CompanyRepository;
 import com.vladislav.crm.services.operations.DeleteOperation;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -9,27 +10,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DeleteCompanyOperationImpl implements DeleteOperation<Company> {
 
     private final EntityManager entityManager;
+    private final CompanyRepository companyRepository;
+
+    @Override
+    public void execute(@NonNull Long id) {
+        remove(companyRepository.findById(id).orElseThrow(EntityNotFoundException::new));
+    }
 
     @Override
     @Transactional
-    public void execute(@NonNull Long id) {
-        entityManager.flush();
-        entityManager.clear();
+    public void execute(Company entity) {
+        remove(entityManager.merge(entity));
+    }
 
-        final Query query = entityManager.createQuery(
-                "update Contact contact set contact.company = null where contact.company.id = :id");
-        query.setParameter("id", id);
-        query.executeUpdate();
-
-        final Query deleteQuery = entityManager.createQuery("delete from Company c where c.id = :id");
-        deleteQuery.setParameter("id", id);
-        deleteQuery.executeUpdate();
+    private void remove(Company persistedCompany) {
+        persistedCompany.getContacts().forEach(contact -> contact.setCompanyUnsafe(null));
+        companyRepository.save(persistedCompany);
     }
 }
