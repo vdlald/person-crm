97d49ac0fa6ccbbf406cfcb321ef5c1a5f2106e1
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-11-09 21:25:25 +0300

    amend

--- src/main/java/com/vladislav/crm/services/operations/companies/impl/DeleteCompanyOperationImpl.java
+++ src/main/java/com/vladislav/crm/services/operations/companies/impl/DeleteCompanyOperationImpl.java
@@ -3,22 +3,22 @@ package com.vladislav.crm.services.operations.companies.impl;
 import com.vladislav.crm.entities.Company;
 import com.vladislav.crm.repositories.CompanyRepository;
 import com.vladislav.crm.services.operations.DeleteOperation;
+import com.vladislav.crm.services.operations.ReadOperation;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DeleteCompanyOperationImpl implements DeleteOperation<Company> {
 
     private final CompanyRepository companyRepository;
+    private final ReadOperation<Company> readCompanyOperation;
 
     @Override
     public void execute(@NonNull Long id) {
-        final Company company = companyRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        final Company company = readCompanyOperation.execute(id);
         company.getContacts().forEach(contact -> contact.setCompanyUnsafe(null));
         companyRepository.save(company);
     }
